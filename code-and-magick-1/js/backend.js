a431f20010f60769ab9a96bb9c8a07417667ec16
Dorofeev-Aleksey <mr.dorick@yandex.ru>
2018-02-19 16:56:20 +0300

    3333

--- code-and-magick-1/js/backend.js
+++ code-and-magick-1/js/backend.js
@@ -1,7 +1,7 @@
 'use strict';
 
 (function () {
-  var SERVER_URL = 'https://1510.dump.academy/code-and-magick';
+  var SERVER_URL = 'https://js.dump.academy/code-and-magick';
   var TIMEOUT = 10000;
   var alertMessages = {
     errorLoad: 'Произошла ошибка',
